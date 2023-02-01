d38eb1a6349da973f24652b8054e7043b06e4430
markon1i <markoschneider34@gmail.com>
2023-02-01 12:21:10 -0300

    inde

--- api/index.js
+++ api/index.js
@@ -22,8 +22,8 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 4000;
 
 // Syncing all the models at once.
-conn.sync({ force: false}).then(() => {
+/* conn.sync({ force: false}).then(() => { */
   server.listen(port, () => {
     console.log(`${port}`); // eslint-disable-line no-console
   });
-});
+/* }); */
